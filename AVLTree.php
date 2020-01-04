763708af770a6967e430e84c76d0955089c385a1
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2020-01-04 16:05:32 +0200

    AVLTree 1.0 Добавлена поддержка нового Qero

--- AVLTree.php
+++ AVLTree.php
@@ -7,9 +7,8 @@
  * (at your option) any later version.
  * 
  * @package     AVLTree
- * @copyright   2019 Podvirnyy Nikita (KRypt0n_)
+ * @copyright   2019 - 2020 Podvirnyy Nikita (Observer KRypt0n_)
  * @license     GNU GPLv3 <https://www.gnu.org/licenses/gpl-3.0.html>
- * @license     Enfesto Studio Group license <https://vk.com/topic-113350174_36400959>
  * @author      Podvirnyy Nikita (KRypt0n_)
  * 
  * Contacts:
@@ -18,9 +17,7 @@
  * VK:    vk.com/technomindlp
  *        vk.com/hphp_convertation
  * 
- */
-
-/**
+ * 
  * @example
  * 
  * <?php
